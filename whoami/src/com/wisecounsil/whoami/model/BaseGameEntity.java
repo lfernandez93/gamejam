4151a0fe3e085c5fe4d2fa9f5088539cc81e88f0
Luis sergio <lsfernandez@urbe.edu.ve>
2014-01-25 01:01:02 -0430

    mejorando la clase de basegameentity

--- whoami/src/com/wisecounsil/whoami/model/BaseGameEntity.java
+++ whoami/src/com/wisecounsil/whoami/model/BaseGameEntity.java
@@ -1,8 +1,40 @@
 package com.wisecounsil.whoami.model;
 
-	interface  BaseGameEntity {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+	
+    public abstract  class BaseGameEntity {
  	
-		public void update();
+		 private Vector2 position;
+		 public Vector2 getPosition() {
+			return position;
+		}
+
+		public void setPosition(Vector2 position) {
+			this.position = position;
+		}
+
+		private Vector2 dimension;
+		 
+		 public Vector2 getDimension() {
+			return dimension;
+		}
+
+		public void setDimension(Vector2 dimension) {
+			this.dimension = dimension;
+		}
+
+		public BaseGameEntity(){
+			 position = new Vector2();
+			 dimension = new Vector2();
+		 }
+		 
+		 public abstract void update();
+		 
+		 public abstract void render(SpriteBatch batch);
+		 
+		 
 		
 	}
 
